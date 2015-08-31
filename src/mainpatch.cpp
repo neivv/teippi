6a4138ea0e2d9b29671e458b71e06683a3ffe465
Markus Heikkinen <ittevien@gmail.com>
2015-08-31 16:28:53 +0300

    Create unique win32 event that can be used to tell if the hack is applied

--- src/mainpatch.cpp
+++ src/mainpatch.cpp
@@ -137,8 +137,23 @@ static void InitFreezeLogging()
     }
 }
 
+static void CreateIdentifyingEvent()
+{
+    char buf[32];
+    auto pid = GetCurrentProcessId();
+    snprintf(buf, sizeof buf, "Teippi #%d", pid);
+    CreateEventA(NULL, FALSE, FALSE, buf);
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+        // Maybe should silently abort patching?
+        MessageBoxA(0, "Second copy of Teippi has been loaded to same process.\n\
+Crashing is very likely.", "Very important warning", 0);
+    }
+}
+
 void InitialPatch()
 {
+    CreateIdentifyingEvent();
     InitLogs();
     InitSystemInfo();
     InitPerfClockFrequency();
